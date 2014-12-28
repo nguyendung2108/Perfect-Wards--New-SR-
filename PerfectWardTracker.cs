0b84acc5987798ba6bcc8fcdf14e20b5f90dee99
drunkenninja <drunken_ninja@hotmail.com>
2014-12-28 20:55:47 +0200

    Update PerfectWardTracker.cs

--- PerfectWardTracker.cs
+++ PerfectWardTracker.cs
@@ -38,7 +38,7 @@ namespace PerfectWard
         void Game_OnGameUpdate(EventArgs args)
         {
 
-            spellbook wardSpellSlot = null;
+            InventorySlot wardSpellSlot = null;
             if (Config.Item("placekey").GetValue<KeyBind>().Active)
             {
                 wardSpellSlot = Items.GetWardSlot();
